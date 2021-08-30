304b6ec6656727571a946d65fc294e0f6a65021b
Scott Donnan <scott.donnan@me.com>
2021-08-30 16:07:18 -0500

    commit tree 9f6c617e4657331244c09a90457a8bb84a1e8263

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(length){
+  let cost;
+  if (length <= 400) {
+    return cost = 'This one is on me!';
+  }
+    else if (length > 2000 && length < 2500) {
+      return cost = 'I will gladly take your thirty bucks.';
+    }
+      else {
+        return cost = 'No can do.';
+      }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? ('Ok, sounds good.') : ('No go.');
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let tipAnswer;
+  switch (tip) {
+    case 'generous' : return tipAnswer = 'Thank you so much.'
+    case 'not as generous' : return tipAnswer = 'Thank you.'
+    default : return tipAnswer = 'Bye.'
+  }
 }
